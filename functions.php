34ac394946144613c488c47652a89a5b62a5a4e1
Srijan Shankar Dubey <srijanshankardubey99@gmail.com>
2022-01-06 16:21:42 +0530

    Student Management System

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+	function clean($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		return $data;
+	}
+
+	function showPrompt() {
+		echo "<div class='alert alert-success'>".$_SESSION['prompt']."</div>";
+	}
+
+	function showError() {
+		echo "<div class='alert alert-danger'>".$_SESSION['errprompt']."</div>";
+	}
